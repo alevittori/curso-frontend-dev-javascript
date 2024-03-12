44e4f420572fb37cf675c091431a1aa2910b902d
alevitt <alejandrovitt@hotmail.com>
2024-03-12 12:49:45 -0300

    modifique la funcion de cargar los productos de la lista, para poder agregarle a la imagen un addEventListener

--- main.js
+++ main.js
@@ -10,6 +10,11 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')//icono d
 
 const asideComprasDetail = document.querySelector('#shoppingCartContainer') // aside que aparece con al descripcion del carrito de compras cuando hacemos click en el carrito
 
+const asideProductDetail = document.querySelector('#productDetail'); // aside que contiene los detalles y descripcion del producto que es seleccionado de la lista de productos. 
+
+
+
+
 // Esconde o muestra el menu cuando hacemos click en el mail del uduario en la vercion de escritorio 
 const togleDesktopMenu = () => {
     const isCarritoMenuClouse = asideComprasDetail.classList.contains('inactive');
@@ -94,23 +99,52 @@ function renderProductList(arr){
     // for of para los elementos 
     for (product of arr) {
     
-        const card = document.createRange().createContextualFragment(`
-        <div class="product-card">
-            <img src= ${product.image} alt="">
-             <div class="product-info">
-                <div>
-                    <p>${product.price}</p>
-                    <p>${product.name}</p>
-                </div>
-            <figure>
-                <img src="./icons/bt_add_to_cart.svg" alt="">
-            </figure>
-            </div>
-        </div>
-        `)
+        // const card = document.createRange().createContextualFragment(`
+        // <div class="product-card">
+        //     <img src= ${product.image} alt="">
+        //      <div class="product-info">
+        //         <div>
+        //             <p>${product.price}</p>
+        //             <p>${product.name}</p>
+        //         </div>
+        //     <figure>
+        //         <img src="./icons/bt_add_to_cart.svg" alt="">
+        //     </figure>
+        //     </div>
+        // </div>
+        // `)
     
-        cardProducContainerElement.append(card)
+        const cardElement = document.createElement('div');
+        const imgProductElement = document.createElement('img');
+        const priceProductElement = document.createElement('p');
+        const nameProductElement = document.createElement('p');
+        const productInfoContainElement = document.createElement('div');
+        const figureBtAddToCartContainerElement = document.createElement('figure');
+        const imgIconAddToCartElement = document.createElement('img');
+
+
+
+        cardElement.classList.add('product-card');//<div class="product-card">
+        imgProductElement.src = product.image;//<img src= ${product.image} alt="">
+        productInfoContainElement.classList.add('product-info');//<div class="product-info">
+        priceProductElement.innerText = `$ ${product.price}`;
+        nameProductElement.innerText = `$ ${product.name}`;
+        imgIconAddToCartElement.src = "./icons/bt_add_to_cart.svg"; //<img src="./icons/bt_add_to_cart.svg" alt="">
+
+        figureBtAddToCartContainerElement.append(imgIconAddToCartElement);// agregamos el icon bt add cart al contenedor figure
+         productInfoContainElement.append(priceProductElement, nameProductElement,figureBtAddToCartContainerElement) //agregamos el precio y el nombre dentro del contenedor produc-info
+
+        cardElement.append(imgProductElement,productInfoContainElement);
+
+
+        cardProducContainerElement.append(cardElement)
     }
     
 }
 renderProductList(productList)
+
+
+// FUNCION PARA ABRIR EL ASIDE DE DETALELS DE CADA PRODUCTO CUANDO LE HACEMOS CLICK EN LA IMAGEN DEL PRODUCTO 
+function openProductDetail (){
+    asideProductDetail.classList.remove('inactive');
+}
