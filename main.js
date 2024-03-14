5ae8500a321f6d9cbc304b0c269a948cb1095e52
alevittHome <alejandrovitt@hotmail.com>
2024-03-14 17:27:50 -0300

    creamos en la funcion de abrir el aside con el detalle del producto, que se nos cierren todos los menus abiertos

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 
 
-const navbar_email_element = document.querySelector('.navbar-email');// selecionamos el elemento que contiene el mail de l usuario
-const desktopMenuElement = document.querySelector('.desktop-menu');// Seleccionamos el elemento que contiene el menu en la version escritorio
 
-const hamburguerIconElement = document.querySelector('#hamIcon'); //Seleccionamos el icono hamburuqesa
-const mobileMenuElement = document.querySelector('.mobile-menu'); // Seleccionamos el menu en su version mobile
+// selecionamos el elemento que contiene el mail de l usuario
+const navbar_email_element = document.querySelector('.navbar-email');
 
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')//icono del carrito de compras
+// Seleccionamos el elemento que contiene el menu en la version escritorio
+const desktopMenuElement = document.querySelector('.desktop-menu');
 
-const asideComprasDetail = document.querySelector('#shoppingCartContainer') // aside que aparece con al descripcion del carrito de compras cuando hacemos click en el carrito
+//Seleccionamos el icono hamburuqesa
+const hamburguerIconElement = document.querySelector('#hamIcon'); 
 
-const asideProductDetail = document.querySelector('#productDetail'); // aside que contiene los detalles y descripcion del producto que es seleccionado de la lista de productos. 
+// Seleccionamos el menu en su version mobile
+const mobileMenuElement = document.querySelector('.mobile-menu'); 
 
+//icono del carrito de compras
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 
+// aside que aparece con al descripcion del carrito de compras cuando hacemos click en el carrito
+const asideComprasDetail = document.querySelector('#shoppingCartContainer') 
+
+// aside que contiene los detalles y descripcion del producto que es seleccionado de la lista de productos. 
+const asideProductDetail = document.querySelector('#productDetail'); 
+
+//boton que cierra el aside de la descripcion del producto
+const btn_clouse_product_detail = document.querySelector('#btn_clouse_productDetail');
 
 
 // Esconde o muestra el menu cuando hacemos click en el mail del uduario en la vercion de escritorio 
@@ -22,7 +33,10 @@ const togleDesktopMenu = () => {
     if (!isCarritoMenuClouse) {
         asideComprasDetail.classList.add('inactive');
     }
-    desktopMenuElement.classList.toggle('inactive')
+    desktopMenuElement.classList.toggle('inactive');
+    
+    // cerramos el aside con la descripcion del producto 
+    cerrarProductDetail();
 }
 // creamos la misma funcion para poner la clase incative pero ahora para cuando hagomos clik en el menu hamburguesas
 const toggleMobileMenu = () => {
@@ -33,7 +47,8 @@ const toggleMobileMenu = () => {
     }
 
     mobileMenuElement.classList.toggle('inactive')
-    // console.log('cliek hamburguesa')
+     // cerramos el aside con la descripcion del producto 
+     cerrarProductDetail();
 }
 
 // funcion para poner la clase inactive al icono del carrito 
@@ -50,8 +65,9 @@ const toggleCarritoIcon = () => {
 
     asideComprasDetail.classList.toggle('inactive');
 
+     // cerramos el aside con la descripcion del producto 
+     cerrarProductDetail();
 }
-
 
 
 //creamos una funcion mas generica para agregar la clase inactive y poder usarla con otros elementos
@@ -59,9 +75,13 @@ const toggleCarritoIcon = () => {
 //     element.classList.tooggle('inactive');
 // }
 
+// escuchamos si el usuario hace click en el mail de l usaurio en la barra de navegacion
+navbar_email_element.addEventListener('click', togleDesktopMenu);
 
-navbar_email_element.addEventListener('click', togleDesktopMenu); // escuchamos si el usuario hace click en el mail de l usaurio en la barra de navegacion
+// escuchamos si el usuario hace click en ICONO HAMBURGUEZA  en la barra de navegacion
 hamburguerIconElement.addEventListener('click', toggleMobileMenu);
+
+// escuchamos si el usuario hace click en EL CARRITO DE COMPRAS  en la barra de navegacion
 menuCarritoIcon.addEventListener('click', toggleCarritoIcon);
 
 //array de la lista de producto cuando se hiciera una consulta al servidor pidiendo la lista de productos, que luego cargaremos en el html
@@ -75,24 +95,23 @@ productList.push({
 productList.push({
     name: 'Escritorio',
     price: 180,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image: "https://images.pexels.com/photos/265129/pexels-photo-265129.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 })
 productList.push({
     name: 'Computador',
     price: 200,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    image: "https://media.istockphoto.com/id/1394988455/es/foto/port%C3%A1til-con-pantalla-en-blanco-sobre-fondo-blanco.jpg?s=2048x2048&w=is&k=20&c=Mw1-pF7aAe5Y4KK4VdlVoLehx5y-0gQAMNfNlwoJ6Qc=",
 })
 
+// CONTENEDOR DE LAS TARJETAS DE LOS PRODUCTOS 
 const cardProducContainerElement = document.querySelector('.cards-container');
 
 
 
-console.log(productList)
-
 // for in para los indices del arreglo 
-for (indice in productList) {
-    console.log(indice)
-}
+// for (indice in productList) {
+//     console.log(indice)
+// }
 
 // FUNCION PARA CARGAR LA LISTA DE PRODUCTOS A LA PAGINA A PARTIR DE UN ARRAY DE PRODUCTOS
 function renderProductList(arr){
@@ -126,11 +145,12 @@ function renderProductList(arr){
 
         cardElement.classList.add('product-card');//<div class="product-card">
         imgProductElement.src = product.image;//<img src= ${product.image} alt="">
+        imgProductElement.addEventListener('click', openProductDetail)
         productInfoContainElement.classList.add('product-info');//<div class="product-info">
         priceProductElement.innerText = `$ ${product.price}`;
         nameProductElement.innerText = `$ ${product.name}`;
         imgIconAddToCartElement.src = "./icons/bt_add_to_cart.svg"; //<img src="./icons/bt_add_to_cart.svg" alt="">
-
+        
         figureBtAddToCartContainerElement.append(imgIconAddToCartElement);// agregamos el icon bt add cart al contenedor figure
          productInfoContainElement.append(priceProductElement, nameProductElement,figureBtAddToCartContainerElement) //agregamos el precio y el nombre dentro del contenedor produc-info
 
@@ -141,10 +161,45 @@ function renderProductList(arr){
     }
     
 }
-renderProductList(productList)
+
+// CARGAMOS LOS PRODUCTOS A LA PAGINA 
+renderProductList(productList);
 
 
 // FUNCION PARA ABRIR EL ASIDE DE DETALELS DE CADA PRODUCTO CUANDO LE HACEMOS CLICK EN LA IMAGEN DEL PRODUCTO 
-function openProductDetail (){
+function openProductDetail (event){
+
     asideProductDetail.classList.remove('inactive');
+    
+    // cerramos todos los menus 
+    cerrarTodosLosMenus();
+
+
+
+    const parent = event.target.parentNode;
+    const lista = parent.childNodes
+    console.log(event)
+    console.log(parent)
+    console.log(lista)
+    // const productDetail = document.querySelector("#productDetail");
+    // parent.append(productDetail)
+
+    
+
+
 }
+// FUNCION PARA CERRAR EL ASIDE CON LA DESCRIPCION DEL PRUDUCTO CUANDO HACEN CLICK EN CEL BOTON DE CERRAR 
+const cerrarProductDetail = () =>{
+    asideProductDetail.classList.add('inactive');
+}
+
+// FUNCION PARA CERRAR TODOS LOS MENUS 
+function cerrarTodosLosMenus (){
+    desktopMenuElement.classList.add('inactive');
+ mobileMenuElement.classList.add('inactive');
+ asideComprasDetail.classList.add('inactive');
+}
+// PARA CERRAR EL ASIDE DE DESCRIPCION DE CADA PRODUCTO 
+btn_clouse_product_detail.addEventListener('click',cerrarProductDetail);
+
+// HACER UNA FUNCION QUE CARGUE EL PRODUCTO QUE SE SELECCIONE EN EL ASIDE DE DESCRIPCION DE PRODUCTO 
